a32ca281543faf7872d300e67afd7e65a4bc12ee
Ronald Wiedijk <ronaldwiedijk@gmail.com>
2014-12-01 20:27:09 +0100

    contact form

--- SofosoftShop.Web/Controllers/HomeController.cs
+++ SofosoftShop.Web/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SofosoftShop.Model;
+using SofosoftShop.Web.Services;
 
 namespace SofosoftShop.Web.Controllers
 {
@@ -31,6 +32,15 @@ namespace SofosoftShop.Web.Controllers
         [HttpPost]
         public ActionResult Contact(Contact model)
         {
+
+            var svc = new MailService();
+            var msg = string.Format("Comment from: {1}{0}Email:{2}{0}Website{3}{0}", Environment.NewLine, model.Name, model.Email, model.Phone,model.Message);
+
+
+            if (svc.SendMail("noreply@whaterver.com", "foo@gmail.com", "Website contact", msg))
+            {
+                ViewBag.MailSent = true;
+            } 
 
             return View();
         }
---
+++ SofosoftShop.Web/Services/IMailService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace SofosoftShop.Web.Services
+{
+   public interface IMailService
+    {
+        bool SendMail(string from, string to, string subject, string body);
+    }
+}
